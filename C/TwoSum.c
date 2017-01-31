34deffcdd8fbcaaa6a927e8e0b18474701ace26b
Shun Lin <s92014721@berkeley.edu>
2017-01-30 17:51:04 -0800

    more codes

--- C/TwoSum.c
+++ C/TwoSum.c
@@ -3,22 +3,31 @@
  * You may assume that each input would have exactly one solution.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target) {
-    int i = 0;
-    int j = 1;
-    int sum = nums[0] + nums[1];
-    while (target != sum) {
-            if (j == numsSize - 1) {
-                    i += 1;
-                    j = i + 1;
-            }
-            else { 
-                    j += 1;
-            }
-            sum = nums[i] + nums[j];
-    }
-    int* p = malloc(2 * sizeof(int));
-    *p = i;
-    *(p + 1) = j;
-    return p;
-}
+ 
+ #include <stdlib.h>
+
+ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+     int tlen = nums1Size + nums2Size;
+     int* nums = malloc(tlen * sizeof(int));
+     memcpy(nums, nums1, nums1Size * sizeof(int));
+     memcpy(nums + nums1Size, nums2, nums2Size * sizeof(int));
+
+      int compare( const void* a, const void* b) {
+      int int_a = * ( (int*) a );
+      int int_b = * ( (int*) b );
+
+      if ( int_a == int_b ) return 0;
+      else if ( int_a < int_b ) return -1;
+      else return 1;
+      }
+
+      qsort(nums, tlen, sizeof(int), compare);
+
+      if (tlen % 2 != 0) {
+          return nums[tlen / 2];
+      }
+      else {
+          return (float) ((nums[tlen / 2 - 1] + nums[tlen / 2])) / 2;
+      }
+
+ }
