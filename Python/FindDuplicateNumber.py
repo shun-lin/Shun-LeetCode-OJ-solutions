b42124cbee1378952a1dbbdd01afe70f0d42b72c
Shun Lin <s92014721@berkeley.edu>
2017-06-12 21:26:38 -0700

    added FindDuplicateNumber.py

---
+++ Python/FindDuplicateNumber.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums);
+        slow = 0;
+        fast = 0;
+        finder = 0;
+        while (True):
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast):
+                break;
+        while (True):
+            slow = nums[slow];
+            finder = nums[finder];
+            if (slow == finder):
+                return slow;
